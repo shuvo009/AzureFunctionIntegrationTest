11ace9807260164f2768c5611bdc2e2117c6bb3d
Hasanuzzaman <shuvo009@yahoo.com>
2021-01-24 15:46:22 +0600

    Host File

--- EmailSendingFunction/EmailSendingFunction.cs
+++ EmailSendingFunction/EmailSendingFunction.cs
@@ -1,15 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using EmailSendingFunction.Core.Interface;
+using EmailSendingFunction.Core.Model;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace EmailSendingFunction
 {
-    public static class EmailSendingFunction
+    public class EmailSendingFunction
     {
-        [FunctionName("SendEmail")]
-        public static void Run([ServiceBusTrigger("emailsendqueue", Connection = "")]
-            string myQueueItem, ILogger log)
+        private readonly IEmailSender _emailSender;
+
+        public EmailSendingFunction(IEmailSender emailSender)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            _emailSender = emailSender;
+        }
+
+        [FunctionName("SendEmail")]
+        public async Task SendEmail([ServiceBusTrigger("emailsendqueue", Connection = "MessageQueueSetting:ConnectionString")]
+            string message, ILogger log)
+        {
+            log.LogInformation($"ServiceBus queue trigger function processed message: {message}");
+            try
+            {
+                var emailModel = JsonConvert.DeserializeObject<EmailModel>(message);
+                await _emailSender.Send(emailModel);
+            }
+            catch (Exception e)
+            {
+                log.LogError(0, e.Message, e);
+            }
         }
     }
 }
---
+++ EmailSendingFunction/Startup.cs
@@ -0,0 +1,25 @@
+﻿using EmailSendingFunction;
+using EmailSendingFunction.Core.Interface;
+using EmailSendingFunction.Service;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace EmailSendingFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            RegisterDependency(builder.Services);
+        }
+
+        private void RegisterDependency(IServiceCollection services)
+        {
+            services.AddTransient<IEmailSender, SendGridEmailSender>();
+        }
+    }
+}
